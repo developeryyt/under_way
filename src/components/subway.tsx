e7638dbd0188fafd31c79e11c7cfbd4daf3c764c
yet1009 <yet1009@hanmail.net>
2025-01-14 12:03:02 +0900

    ..

--- src/components/subway.tsx
+++ src/components/subway.tsx
@@ -1,7 +1,7 @@
 
 
 const NowLocation = async () => {
-    const result = await fetch(`${process.env.NEXT_PUBLIC_SUBWAY_API_ADDRESS}/api/subway/${process.env.NEXT_PUBLIC_OPENAPI_SUBWAY_KEY}/json/realtimeStationArrival/0/5/서울`);
+    const result = await fetch(`${process.env.NEXT_PUBLIC_SUBWAY_API_ADDRESS}/api/subway/${process.env.NEXT_PUBLIC_OPENAPI_SUBWAY_KEY}/json/realtimeStationArrival/0/5/잠실새내`);
     const data = await result.json();
 
     console.log(data)
@@ -15,7 +15,7 @@ const NowLocation = async () => {
 
 const Subway = () => {
     return (
-        <div>
+        <div className=''>
             <NowLocation />
         </div>
     )
